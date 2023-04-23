dd7699e0ef484f376517b15785dfe85d5fad3ae8
Eric Daumler <daumlereric@hotmail.com>
2023-04-23 13:42:38 -0300

    creacion de firebese y su conexion

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,14 +8,28 @@ import { Infopagina } from '../interface/info-pagina.interface';
 export class InfoPaginaService {
   info : Infopagina = {};
   cargada = false;
+  fireb:any[]=[];
 
   constructor(private http: HttpClient) { 
-    this.http.get('assets/data/data.json')
-      .subscribe((resp: Infopagina) =>{
-        this.cargada=true;
-        this.info=resp;
-        console.log(resp)
-      })
+    this.cargarData();
+    this.cargar_Firebase();
+            
+  }
 
+  private cargar_Firebase(){
+
+  this.http.get('https://angular-html-5bc66-default-rtdb.firebaseio.com/equipo.json')
+  .subscribe((resp:any)=>{
+    this.fireb=resp;
+    console.log(resp);
+  })
+  }
+  private cargarData(){
+  
+  this.http.get('assets/data/data.json')
+  .subscribe((resp: Infopagina) =>{
+    this.cargada=true;
+    this.info=resp;    
+    })
   }
 }
