ad99893f523dd68688cf891c0324fa713133b474
Eric Daumler <daumlereric@hotmail.com>
2023-04-24 11:40:24 -0300

    cargando.portafolio y porductosidx.json

--- src/app/interface/info_prod.interface.ts
+++ src/app/interface/info_prod.interface.ts
@@ -1,6 +1,6 @@
 export interface Productos {
-    categoria: string;
-    cod: string;
-    titulo: string;
-    url: string;
+    categoria?: string;
+    cod?: string;
+    titulo?: string;
+    url?: string;
   }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,20 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Productos } from '../interface/info_prod.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
   cargar=true;
+  productos:Productos[]=[];
   
   constructor(private http: HttpClient) {
     this.Cargar_producto()
    }
    private Cargar_producto(){
     this.http.get('https://angular-html-5bc66-default-rtdb.firebaseio.com/productos_idx.json')
-    .subscribe((resp)=> {
-      console.log(resp)
-      this.cargar=false 
+    .subscribe((resp:Productos)=> {
+      this.productos=Object.values(resp)
+      console.log(this.productos)
+      setTimeout(()=>{
+        this.cargar=false 
+        
+      },2000)
     });
    }
 
